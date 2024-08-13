5cf110a88dac5fc849563e768840976443f15fc8
Emil Atanasov <heitara@gmail.com>
2024-08-13 03:20:39 +0300

    feat: revert to spreadsheet

--- src/containers/applications/apps/spreadsheet.jsx
+++ src/containers/applications/apps/spreadsheet.jsx
@@ -37,30 +37,30 @@ export const Spreadsheet = () => {
     "reset",
   ]);
 
-  //1) setup the diagram engine
-	var engine = createEngine();
+  // //1) setup the diagram engine
+	// var engine = createEngine();
 
-	//2) setup the diagram model
-	var model = new DiagramModel();
+	// //2) setup the diagram model
+	// var model = new DiagramModel();
 
-	//3-A) create a default node
-	var node1 = new DefaultNodeModel('Node 1', 'rgb(0,192,255)');
-	var port1 = node1.addOutPort('Out');
-	node1.setPosition(100, 100);
+	// //3-A) create a default node
+	// var node1 = new DefaultNodeModel('Node 1', 'rgb(0,192,255)');
+	// var port1 = node1.addOutPort('Out');
+	// node1.setPosition(100, 100);
 
-	//3-B) create another default node
-	var node2 = new DefaultNodeModel('Node 2', 'rgb(192,255,0)');
-	var port2 = node2.addInPort('In');
-	node2.setPosition(400, 100);
+	// //3-B) create another default node
+	// var node2 = new DefaultNodeModel('Node 2', 'rgb(192,255,0)');
+	// var port2 = node2.addInPort('In');
+	// node2.setPosition(400, 100);
 
-	//3-C) link the 2 nodes together
-	var link1 = port1.link(port2);
+	// //3-C) link the 2 nodes together
+	// var link1 = port1.link(port2);
 
-	//4) add the models to the root graph
-	model.addAll(node1, node2, link1);
+	// //4) add the models to the root graph
+	// model.addAll(node1, node2, link1);
 
-	//5) load model into engine
-	engine.setModel(model);
+	// //5) load model into engine
+	// engine.setModel(model);
 
   
   return (
@@ -83,8 +83,8 @@ export const Spreadsheet = () => {
         bg="#f9f9f9"
         noinvert
       />
-        {/* <Workbook data={[{ name: "Sheet1" }]} /> */}
-        <CanvasDragToggle engine={engine} model={model} />
+        <Workbook data={[{ name: "Sheet1" }]} />
+        {/* <CanvasDragToggle engine={engine} model={model} /> */}
     </div>
   );
 };
